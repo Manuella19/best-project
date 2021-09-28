ad3fc03e0a2db28f823d86e0cdeeb945530bb965
Manuella19 <aghajanyanmaria2002@mail.ru>
2021-09-29 00:48:56 +0400

    New Text file

---
+++ html1.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Wix</title>
+    </head>
+    <body>
+   <h1>Sign Up</h1>
+<br>
+		
+<div class="form-floating mb-3">
+  <input  type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput"></label>
+</div>
+		
+<br>
+		
+<div class="form-floating">
+  <input type="login" class="form-control" id="floatingPassword" placeholder="username">
+  <label for="floatingPassword"></label>
+</div>
+		
+<br>
+		
+<div>
+  <input  type="password" class="form-control" id="floatingInput" placeholder="password">
+  <label for="floatingInput"></label>
+</div>
+		
+<p>
+<a href="submit.php"><input type="button" value="Sign up"></a>
+
+
+
+		
+		
+    </body>
+</html>
